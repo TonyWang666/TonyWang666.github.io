85cdfacc2f45e5ac0732b30cc818ad1fce77c82b
TonyWang666 <jiaxiaw1@uci.edu>
2019-10-14 23:10:36 -0700

    Updates

---
+++ precache-manifest.a653796199b7483b5f3d67f165e8465f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1761e7ca2b5565ef4d80606dab6d315a",
+    "url": "/TonyWangJX/index.html"
+  },
+  {
+    "revision": "f07d404881edebdfdf59",
+    "url": "/TonyWangJX/static/css/main.2473b5af.chunk.css"
+  },
+  {
+    "revision": "d4581ecc369caa99f4dd",
+    "url": "/TonyWangJX/static/js/2.dbdb784b.chunk.js"
+  },
+  {
+    "revision": "f07d404881edebdfdf59",
+    "url": "/TonyWangJX/static/js/main.cd308211.chunk.js"
+  },
+  {
+    "revision": "c49c80ab8f72c686f079",
+    "url": "/TonyWangJX/static/js/runtime-main.111ecbd9.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/TonyWangJX/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "b986b4fd45fd54c447cd5783082c397f",
+    "url": "/TonyWangJX/static/media/gmail.b986b4fd.svg"
+  },
+  {
+    "revision": "c78ada969ab69dfb6bb5ae2181274d20",
+    "url": "/TonyWangJX/static/media/homePicture.c78ada96.jpg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/TonyWangJX/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "d5a700628dd6877ffd5fd3477e897860",
+    "url": "/TonyWangJX/static/media/resume.d5a70062.pdf"
+  }
+]);
