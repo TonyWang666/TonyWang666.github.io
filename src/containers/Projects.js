82af04edb7f285c2f54b369001a310f7706e7a46
TonyWang666 <jiaxiaw1@uci.edu>
2019-11-04 11:22:24 -0800

    test for adding ! after jQuery

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -18,7 +18,7 @@ const Projects = () => (
       <p className = "des text">The Movie Zone is a website I made in class 122B. Honestly, it is my child. I spent 3 months on this product with all my passion and tons of "hairs"
       <br /><br />I used microservice with MySql for its database. There are 4 databases work separately and independently.
       <br /><br />I implemented the back-end with Java, which is my most proficient language.
-      <br /><br />The front-end I used JavaScript and jQuery.
+      <br /><br />The front-end I used JavaScript and jQuery!
       </p>
     </div>
   </Fragment>
