45cc4703147f6577c5c45c0ece4e0c486508f99e
Jiaxiang Wang <jiaxiangwang@Jiaxiangs-MacBook-Pro.local>
2020-02-06 08:44:20 -0800

    Updates

---
+++ precache-manifest.6a76c714c0d0c21c464e8c8b873996b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873d5df6982b6dff4fcaad4b4bfbf201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee76cbbef8cbbed495bd",
+    "url": "/static/css/main.2473b5af.chunk.css"
+  },
+  {
+    "revision": "56e8ef9213d1ce711766",
+    "url": "/static/js/2.ef214d85.chunk.js"
+  },
+  {
+    "revision": "ee76cbbef8cbbed495bd",
+    "url": "/static/js/main.f5138ff0.chunk.js"
+  },
+  {
+    "revision": "7ec41e30773b6ea259c5",
+    "url": "/static/js/runtime-main.1c9ec464.js"
+  },
+  {
+    "revision": "c78ada969ab69dfb6bb5ae2181274d20",
+    "url": "/static/media/aboutMePicture.c78ada96.jpg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "b986b4fd45fd54c447cd5783082c397f",
+    "url": "/static/media/gmail.b986b4fd.svg"
+  },
+  {
+    "revision": "362f5d859efdbe3b4e4c1f0d9d930ac6",
+    "url": "/static/media/homePicture.362f5d85.jpg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "e98fca66721624620493b20fc7371075",
+    "url": "/static/media/resume.e98fca66.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29ca5dfa76ac2191c4dd090e3d3c929f.js"
+  "/precache-manifest.6a76c714c0d0c21c464e8c8b873996b3.js"
 );
 
 self.addEventListener('message', (event) => {
